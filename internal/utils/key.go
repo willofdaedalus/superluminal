f315cb8d61e86eb2807b5afb3718fafcf2ec2fa1
willofdaedalus <manny.is.daedalus@pm.me>
2024-09-30 22:13:53 +0000

    chore: added comments to key.go funcs

--- internal/utils/key.go
+++ internal/utils/key.go
@@ -7,10 +7,12 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// checks the client entered passphrase with the hashed passphrase
 func CheckPassphrase(hash, passphrase string) bool {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(passphrase)) == nil
 }
 
+// generates a new diceware passphrase
 func GeneratePassphrase() (string, error) {
 	list, err := diceware.Generate(6)
 	if err != nil {
@@ -20,6 +22,7 @@ func GeneratePassphrase() (string, error) {
 	return strings.Join(list, " "), nil
 }
 
+// returns a hashed passphrase
 func HashPassphrase(passphrase string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(passphrase), bcrypt.DefaultCost)
 	if err != nil {

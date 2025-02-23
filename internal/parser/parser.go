dcb157d87647d24220c37c5f426ffa5834da6425
willofdaedalus <manny.is.daedalus@pm.me>
2025-02-23 21:08:59 +0000

    feat(encoding): basic parser with ansi colour encoding

---
+++ internal/parser/parser.go
@@ -0,0 +1,84 @@
+package parser
+
+import (
+	"bytes"
+	"strconv"
+)
+
+func parseTokens(tks []token) []byte {
+	cleanOutput := make([]byte, 0)
+
+	for _, tk := range tks {
+		switch tk.tokenType {
+		case ansiColour:
+			final := parseAnsiColours(tk.content)
+			cleanOutput = append(cleanOutput, final...)
+		case newline:
+			cleanOutput = append(cleanOutput, tk.content...)
+		}
+	}
+
+	return cleanOutput
+}
+
+func parseAnsiColours(content []byte) []byte {
+	var attr byte = 0
+	var fgBright, bgBright byte = 0, 0
+	var fgColor, bgColor byte = 0, 0
+
+	if len(content) == 0 { // Reset case
+		return []byte{ESC, 0, 0, 0} // Just return zeroed attributes
+	}
+
+	tks := bytes.Split(content, []byte{';'})
+	for i := 0; i < len(tks); i++ {
+		num, err := strconv.Atoi(string(tks[i]))
+		if err != nil {
+			continue
+		}
+		switch num {
+		case 0:
+			attr, fgBright, bgBright, fgColor, bgColor = 0, 0, 0, 0, 0
+		case 1:
+			attr |= 1 << 0 // bold
+		case 2:
+			attr |= 1 << 1 // faint
+		case 3:
+			attr |= 1 << 2 // italic
+		case 4:
+			attr |= 1 << 3 // underline
+		case 5, 6:
+			attr |= 1 << 4 // blink
+		case 7:
+			attr |= 1 << 5 // reverse
+		case 9:
+			attr |= 1 << 6 // strikethrough
+
+		// foreground colour
+		case 30, 31, 32, 33, 34, 35, 36, 37:
+			fgColor = byte(num - 30)
+		case 90, 91, 92, 93, 94, 95, 96, 97:
+			// colour codes within the 90s range are reserved for bright
+			// so that instead of writing something like [1;31 for bright red,
+			// it can instead be something like [91 for the same bright red
+			fgColor = byte(num - 90)
+			fgBright = 1
+
+		// background colour handling
+		case 40, 41, 42, 43, 44, 45, 46, 47:
+			bgColor = byte(num - 40)
+		case 100, 101, 102, 103, 104, 105, 106, 107:
+			// same reason for the 90s range, numbers between 100 and 107 are
+			// reserved for bright background colours
+			bgColor = byte(num - 100)
+			bgBright = 1
+		}
+	}
+
+	// Return 3-byte format: [attr][(fgBright<<3)|fgColor][(bgBright<<3)|bgColor]
+	return []byte{
+		ESC, attr,
+		(fgBright << 3) | fgColor,
+		(bgBright << 3) | bgColor,
+	}
+}
---
+++ internal/parser/parser_const.go
@@ -0,0 +1,4 @@
+package parser
+
+type ansiRepresentation struct {
+}
